86d1c330722b3c06d8bd13ad1f30ca47e56309e7
Jarencio <145330400+Jarencio@users.noreply.github.com>
2024-02-27 17:15:44 +0800

    Mysql

---
+++ Student_Management/Student_Management/DBConnect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Student_Management
+{
+    class DBConnect
+    {
+        MySqlConnection connect = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=studentdb");
+
+        public MySqlConnection getconnection
+        {
+            get
+            {
+                return connect;
+            }
+        }
+
+        public void openconnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Closed)
+                connect.Open();
+        }
+
+        public void closeconnect()
+        {
+            if (connect.State == System.Data.ConnectionState.Open)
+                connect.Close();
+        }
+
+    }
+}
